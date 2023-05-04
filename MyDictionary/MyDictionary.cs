21c4efc6f3caabde4b686875d9b13c515dcaa482
fatih0727 <fatih.demiraran07@gmail.com>
2023-05-04 13:23:37 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] dictionary;
+        public MyDictionary()
+        {
+            dictionary = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = dictionary;
+            dictionary = new T[dictionary.Length+1];
+            for (int i = 0; i <tempArray.Length; i++)
+            {
+                dictionary[i] = tempArray[i];
+            }
+            dictionary[dictionary.Length- 1] = item;
+        }
+        public void See()
+        {
+            foreach (T x in dictionary)
+            {
+                Console.WriteLine(x);
+            }
+        }
+       public int Lengths
+        {
+            get { return dictionary.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+
+            myDictionary.Add("Red");
+            myDictionary.Add("Yellow");
+            myDictionary.Add("Pink");
+            Console.WriteLine(myDictionary.Lengths);
+            myDictionary.See();
+        }
+    }
+}
